4cb02f915cdbd3d3174bcd5eea5974e1a0efb036
arkrish1993 <archanarkrishnan3@gmail.com>
2021-12-12 21:35:30 +0530

    typescript types practice

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Your code is here...");
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printResult(n) {
+    console.log("Result: ".concat(n));
+}
+// Function as type
+// let combineValues: Function;
+var combineValues;
+combineValues = add;
+// combineValues = printResult
+// combineValues = 5;
+printResult(combineValues(2, 3));
+//Using unknown type
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string")
+    userName = userInput;
+//Using never type
+function generatError(msg, code) {
+    throw { message: msg, errorCode: code };
+}
+generatError("An error occurred", 500);
--- app.ts
+++ app.ts
@@ -1,1 +1,35 @@
-console.log("Your code is here...");
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(n: number): void {
+  console.log(`Result: ${n}`);
+}
+
+// Function as type
+
+// let combineValues: Function;
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult
+// combineValues = 5;
+
+printResult(combineValues(2, 3));
+
+//Using unknown type
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") userName = userInput;
+
+//Using never type - to be used when you are sure that nothing will ever be returned from the function
+function generatError(msg: string, code: number): never {
+  throw { message: msg, errorCode: code };
+}
+
+generatError("An error occurred", 500);
